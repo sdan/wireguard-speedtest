0c027ede81a58cd8ef84ca7cdfcabfb034c96cb4
Surya <git@sdan.io>
2022-10-20 16:08:12 -0400

    add goroutines and write once at the end

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
+	"runtime"
 	"sort"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/go-ping/ping"
@@ -28,32 +31,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	f, err := os.Create("stats.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	for _, file := range files {
-		log.Println("Files in config directory:", file.Name())
-		filePath := path + "/config/" + file.Name()
-		log.Println("File path:", filePath)
 
-		config, err := wireproxy.ParseConfig(filePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("Config:", config)
-		log.Println("Config.Device:", config.Device)
-		log.Println("Config.Device.Peers:", config.Device.Peers)
-		log.Println("Config.Device.Peers[0]:", config.Device.Peers[0].Endpoint)
-		endpoint := config.Device.Peers[0].Endpoint
-		avgLatency := pingPeer(endpoint)
-		sortedPeers[file.Name()] = avgLatency
-		outputString := "(" + file.Name() + ") " + avgLatency.String() + "\n"
-		fmt.Print(outputString)
-		f.WriteString(outputString)
-		f.Sync()
+	sem := make(chan struct{}, runtime.NumCPU())
+
+	var wg sync.WaitGroup
+	wg.Add(len(files))
+
+	for _, file := range files {
+		go func(file fs.FileInfo) {
+			sem <- struct{}{}
+			defer func() { <-sem }()
+			defer wg.Done()
+
+			log.Println("Files in config directory:", file.Name())
+			filePath := path + "/config/" + file.Name()
+			log.Println("File path:", filePath)
+
+			config, err := wireproxy.ParseConfig(filePath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println("Config:", config)
+			log.Println("Config.Device:", config.Device)
+			log.Println("Config.Device.Peers:", config.Device.Peers)
+			log.Println("Config.Device.Peers[0]:", config.Device.Peers[0].Endpoint)
+			endpoint := config.Device.Peers[0].Endpoint
+			avgLatency := pingPeer(endpoint)
+			sortedPeers[file.Name()] = avgLatency
+			outputString := "(" + file.Name() + ") " + avgLatency.String() + "\n"
+			fmt.Print(outputString)
+		}(file)
 	}
+	wg.Wait()
 
 	// Sort sortedPeers by latency
 	var keys []string
@@ -63,10 +72,24 @@ func main() {
 	sort.Slice(keys, func(i, j int) bool {
 		return sortedPeers[keys[i]] < sortedPeers[keys[j]]
 	})
+
+	f, err := os.Create("stats.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	for _, k := range keys {
+		outputString := "(" + k + ") " + sortedPeers[k].String() + "\n"
+		f.WriteString(outputString)
+		f.Sync()
+	}
+
 	fmt.Println("Top 10 fastest peers:")
 	for i := 0; i < 10; i++ {
 		fmt.Println(strconv.Itoa(i+1)+". ("+keys[i]+")", sortedPeers[keys[i]])
 	}
+	close(sem)
 
 	// 2.
 
