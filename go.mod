6c51bdf87868ae603d9172b5acfbfd4e2f006213
Surya <git@sdan.io>
2022-10-20 02:01:31 -0400

    one punch

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/sdan/wg-netcheck
+
+go 1.19
+
+require (
+	github.com/go-ping/ping v1.1.0
+	github.com/octeep/wireproxy v1.0.5
+)
+
+require (
+	github.com/MakeNowJust/heredoc/v2 v2.0.1 // indirect
+	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5 // indirect
+	github.com/go-ini/ini v1.66.4 // indirect
+	github.com/google/btree v1.0.1 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220315194320-039c03cc5b86 // indirect
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
+	golang.zx2c4.com/wintun v0.0.0-20211104114900-415007cec224 // indirect
+	golang.zx2c4.com/wireguard v0.0.0-20220829161405-d1d08426b27b // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gvisor.dev/gvisor v0.0.0-20220817001344-846276b3dbc5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/go-ping/ping"
+	wireproxy "github.com/octeep/wireproxy"
+)
+
+func main() {
+	// Discards log output, comment out to see log output
+	log.SetOutput(ioutil.Discard)
+
+	sortedPeers := make(map[string]time.Duration)
+
+	// 1. Gets all the files that end in .config path in the config directory
+	files, err := ioutil.ReadDir("./config")
+	if err != nil {
+		log.Fatal(err)
+	}
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.Create("stats.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	for _, file := range files {
+		log.Println("Files in config directory:", file.Name())
+		filePath := path + "/config/" + file.Name()
+		log.Println("File path:", filePath)
+
+		config, err := wireproxy.ParseConfig(filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Config:", config)
+		log.Println("Config.Device:", config.Device)
+		log.Println("Config.Device.Peers:", config.Device.Peers)
+		log.Println("Config.Device.Peers[0]:", config.Device.Peers[0].Endpoint)
+		endpoint := config.Device.Peers[0].Endpoint
+		avgLatency := pingPeer(endpoint)
+		sortedPeers[file.Name()] = avgLatency
+		outputString := "(" + file.Name() + ") " + avgLatency.String() + "\n"
+		fmt.Print(outputString)
+		f.WriteString(outputString)
+		f.Sync()
+	}
+
+	// Sort sortedPeers by latency
+	var keys []string
+	for k := range sortedPeers {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return sortedPeers[keys[i]] < sortedPeers[keys[j]]
+	})
+	fmt.Println("Top 10 fastest peers:")
+	for i := 0; i < 10; i++ {
+		fmt.Println(strconv.Itoa(i+1)+". ("+keys[i]+")", sortedPeers[keys[i]])
+	}
+
+	// 2.
+
+}
+
+// Function that pings the endpoint of the peer and returns the latency
+func pingPeer(endpoint string) time.Duration {
+	// Trim last 6 characters of endpoint to get the IP address
+	endpoint = endpoint[:len(endpoint)-6]
+	log.Println("Pinging peer:", endpoint)
+	pinger, err := ping.NewPinger(endpoint)
+	if err != nil {
+		panic(err)
+	}
+	pinger.Count = 3
+	err = pinger.Run() // Blocks until finished.
+	if err != nil {
+		panic(err)
+	}
+	stats := pinger.Statistics() // get send/receive/duplicate/rtt stats
+	log.Println("Ping stats:", stats.AvgRtt)
+	return stats.AvgRtt
+}
